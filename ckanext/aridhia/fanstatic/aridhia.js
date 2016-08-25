7c00b0efb82b35749c6bf5ff9aafac74f14f0dbe
Petar Efnushev <petar.efnushev@gmail.com>
2016-08-25 10:51:08 +0200

    Toggle datepickers on click

--- ckanext/aridhia/fanstatic/aridhia.js
+++ ckanext/aridhia/fanstatic/aridhia.js
@@ -18,5 +18,21 @@
 		$('#field-maintainer-email').val(current_user_email);
 
 	});
+
+      $(document).ready(function () {
+        $('#tc_start').datepicker({
+          dateFormat: 'yy-mm-dd'
+        });
+        $('#tc_end').datepicker({
+          dateFormat: 'yy-mm-dd'
+        });
+
+        // Required for the info button for the fields
+        $('.control-group-info-toggle').click(function (e) {
+          e.preventDefault();
+          $(this).toggleClass('toggled');
+          $(this).parent().children('.control-group-info-content').toggleClass('control-group-info-content-hidden');
+        });
+      });
 	
  })($);
